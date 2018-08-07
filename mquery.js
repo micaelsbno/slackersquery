b1c89b639fb80a04854f773fbc9039352af40e86
micaelsbno <micaelsbno@gmail.com>
2018-08-07 11:34:26 +1000

    added mquery v0.2

---
+++ mquery.js
@@ -0,0 +1,10 @@
+
+function m$(item) {
+  document.querySelector(item)
+}
+function m$$(item) {
+  document.querySelectorAll(item)
+}
+function m$Create(item) {
+  document.createElement(element)
+}
